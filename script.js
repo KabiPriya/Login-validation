acc899adecdd53c598e06fc1d7579738e8ee9fe4
KabiPriya <kabipriya.t@gmail.com>
2023-10-01 22:04:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    const brr = [];
+    const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    btnSubmit = $("#submit");
+    btnSubmit.click(function (e) {
+      e.preventDefault();
+      const email = $("#username").val();
+      const password = $("#password").val();
+      if (emailRegex.test(email)) {
+        if (passwordRegex.test(password)) {
+          const profile = {
+            email: email,
+            password: password,
+          };
+          brr.push(profile);
+          console.log(JSON.stringify(brr));
+        } else {
+          alert(
+            "Password must contain at least 8 characters, including at least one letter, one digit, and one special character."
+          );
+        }
+      } else {
+        alert("Enter a valid email address.");
+      }
+    });
+  });
